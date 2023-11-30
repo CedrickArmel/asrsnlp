a89aa40b7ebe30d73f818265ec68093a68ae1d1c
CeddrickArmel <35418979+CedrickArmel@users.noreply.github.com>
2023-12-01 00:25:11 +0100

    feat: vmsetup file

---
+++ src/asrsnlp/vmsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PYENV_GIT_TAG=v2.3.28
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+sudo 
