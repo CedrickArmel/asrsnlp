e625b582426032250406c1f749531d05fafe2dc9
CeddrickArmel <35418979+CedrickArmel@users.noreply.github.com>
2023-12-15 05:03:18 +0100

    fix: extract non project specific stufs

--- src/asrsnlp/vmsetup.sh
+++ src/asrsnlp/vmsetup.sh
@@ -1,45 +1,5 @@
 #!/bin/bash
 
-# Install essential build dependencies for Python
-sudo apt update
-sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-# Check if Pyenv is already installed
-if ! command -v pyenv &> /dev/null; then
-    # Install Pyenv with a specific version
-    export PYENV_GIT_TAG=v2.3.28
-    curl https://pyenv.run | bash
-else
-    echo "Pyenv is already installed."
-fi
-
-# Configure git
-if ! command -v git &> /dev/null; then
-    # Install git
-    sudo apt update
-    sudo apt install -y git
-else
-    echo "Git is already installed."
-fi
-
-# Configure environment variables in the user's bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'command -v pyenv >/dev/null || eval "$(pyenv init --path)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-
-# Configure git
-git config --global user.name "CedrickArmel"
-git config --global user.email "35418979+CedrickArmel@users.noreply.github.com"
-chmod 600 ~/.ssh/id_rsa_vms
-# ssh-keygen -t rsa -b 4096 -C "35418979+CedrickArmel@users.noreply.github.com"
-# eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa
-
-# Reload bash to apply changes
-source $HOME/.bashrc
-
 # project specific setup
 setup() {
     local basepath=$1
